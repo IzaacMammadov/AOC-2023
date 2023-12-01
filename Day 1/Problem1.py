11fd3036b7889b102ead145dcc5f4fe776f236d4
Izaac Mammadov <izaacmammadov@tuta.io>
2023-12-01 20:27:43 +0000

    Day 1

---
+++ Day 1/Problem1.py
@@ -0,0 +1,19 @@
+"""Day 1 - Problem 1 of Advent of Code 2023"""
+output = 0
+with open("Day 1/input.txt") as input_file:
+    input_text = input_file.read().splitlines()
+for string in input_text:
+    n = len(string)
+    first_num = None
+    last_num = None
+    for i in range(n):
+        if string[i].isnumeric():
+            first_num = string[i]
+            break
+    for i in range(n - 1, -1, -1):
+        if string[i].isnumeric():
+            last_num = string[i]
+            break
+    num = int(f"{first_num}{last_num}")
+    output += num
+print(output)
---
+++ Day 1/Problem2.py
@@ -0,0 +1,47 @@
+"""Day 1 - Problem 2 of Advent of Code 2023"""
+output = 0
+with open("input.txt") as input_file:
+    input_text = input_file.read().splitlines()
+mapping = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "zero": 0,
+}
+for string in input_text:
+    n = len(string)
+    first_num = Noneq
+    last_num = None
+    for i in range(n):
+        if string[i].isnumeric():
+            first_num = string[i]
+            break
+        double_break = False
+        for j in range(i):
+            if string[j:i + 1] in mapping:
+                first_num = mapping[string[j:i + 1]]
+                double_break = True
+                break
+        if double_break:
+            break
+    for i in range(n - 1, -1, -1):
+        if string[i].isnumeric():
+            last_num = string[i]
+            break
+        double_break = False
+        for j in range(i + 1, n):
+            if string[i:j + 1] in mapping:
+                last_num = mapping[string[i:j + 1]]
+                double_break = True
+                break
+        if double_break:
+            break
+    num = int(f"{first_num}{last_num}")
+    output += num
+print(output)
