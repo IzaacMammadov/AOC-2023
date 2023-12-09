8f7a451b306f9b5bb1d5d30949fb642a5ec8b544
Izaac Mammadov <izaacmammadov@tuta.io>
2023-12-09 16:48:54 +0000

    Day 9

---
+++ Day 9/Problem1.py
@@ -0,0 +1,15 @@
+"""Day 9 - Problem 1 of Advent of Code 2023"""
+from numpy import diff
+
+with open("input.txt") as input_file:
+    input_text = input_file.read().splitlines()
+output = 0
+for line in input_text:
+    nums = [[int(i) for i in line.split()]]
+    while any(i != 0 for i in nums[-1]):
+        nums.append(list(diff(nums[-1])))
+    next_diff = 0
+    while nums:
+        next_diff += nums.pop()[-1]
+    output += next_diff
+print(output)
